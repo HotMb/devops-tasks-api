62ee0c683a4799d543767dd512af5431ed11df6a
MabWalid <mabroukwalid0@gmail.com>
2026-06-02 22:21:20 +0200

    ajoute des tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,16 +9,24 @@ def test_health():
 def test_add_task():
     client = app.test_client()
     response = client.post('/tasks', json={"title": "Test"})
-    assert response.status_code == 201
+    assert response.status_code == 200
     assert "id" in response.json
 
 def test_delete_task():
     client = app.test_client()
+
+    # Création
     response = client.post('/tasks', json={"title": "Test"})
-    assert response.status_code == 200
+    assert response.status_code == 201
     assert "id" in response.json
-    response = client.delete(f'/tasks/{response.json["id"]}')
-    assert response.status_code == 204
+
+    task_id = response.json["id"]
+
+    # Suppression
+    response = client.delete(f'/tasks/{task_id}')
+    assert response.status_code == 200
+
+    # Vérification
     response = client.get('/tasks')
     assert response.status_code == 200
     assert len(response.json) == 0
